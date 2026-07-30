5d3d578b011ce4d9190543832039f24f9fb974bb
WeiShenL <79853365+WeiShenL@users.noreply.github.com>
2026-07-30 21:47:57 +0800

    fix: replace mock firebase apikey

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo "Injecting VPS environment variables into static JS bundle..."
 
 find /usr/share/caddy -type f -name "*.js" | while read -r file; do
   if [ -f "$file" ]; then
-    [ -n "$EXPO_PUBLIC_FIREBASE_API_KEY" ] && sed -i "s|__EXPO_PUBLIC_FIREBASE_API_KEY__|${EXPO_PUBLIC_FIREBASE_API_KEY}|g" "$file"
+    [ -n "$EXPO_PUBLIC_FIREBASE_API_KEY" ] && sed -i "s|AIzaSy_BUILD_MOCK_FIREBASE_KEY_PLACEHOLDER_39CH|${EXPO_PUBLIC_FIREBASE_API_KEY}|g" "$file"
     [ -n "$EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN" ] && sed -i "s|__EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN__|${EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN}|g" "$file"
     [ -n "$EXPO_PUBLIC_FIREBASE_PROJECT_ID" ] && sed -i "s|__EXPO_PUBLIC_FIREBASE_PROJECT_ID__|${EXPO_PUBLIC_FIREBASE_PROJECT_ID}|g" "$file"
     [ -n "$EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET" ] && sed -i "s|__EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET__|${EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET}|g" "$file"
